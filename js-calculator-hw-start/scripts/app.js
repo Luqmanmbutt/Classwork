4c3ec33d198238dc80fd8194fade53a8e8ede588
Luqman Butt <luqmanmbutt1@gmail.com>
2022-10-12 17:27:13 +0100

    added new class work

---
+++ js-calculator-hw-start/scripts/app.js
@@ -0,0 +1,5 @@
+// ! 💻 Remember when working in the browser, be sure to save the changes in this file, and refresh the browser window to run the code again.
+
+// ! 👨‍🏫 Read the readme carefully, and practice using "window.prompt()" to gather user input
+
+// * Write your code below!
