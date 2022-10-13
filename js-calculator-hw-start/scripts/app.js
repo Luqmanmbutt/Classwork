1e247a3849a002add4c6fa9706071ef58935ef09
Luqman Butt <luqmanmbutt1@gmail.com>
2022-10-13 13:28:45 +0100

    finished calculator project javascript

--- js-calculator-hw-start/scripts/app.js
+++ js-calculator-hw-start/scripts/app.js
@@ -3,15 +3,50 @@
 // ! 👨‍🏫 Read the readme carefully, and practice using "window.prompt()" to gather user input
 
 // * Write your code below!
+
+// pseudocode 
+
+// --  if plus is true add first number and second number for result
+// -- if signs equls plus then add firstNumber and Second number for equation
+// 3 types - 1. string ('firstNumber') 2. boolean ( true or false ) 3. number ( 3 )
+
+
+
+
 function init() {
 
-    var firstValue = prompt("What is your first value?");
+    let firstNumber = prompt("what is the first number?");
+    console.log( firstNumber);
+    let secondNumber = prompt("What is your second number?")
+    console.log( secondNumber);
+    let signs = prompt("Would you like to + , - , * or /")
+    console.log (signs);
+    let message = "YOUR RESULT  -  "
 
-    if (answer == 10) {
-      alert("Wow! Correct!");
-    } else {
-      alert("Wrong!")
-    }
+
+    
+    if (signs === "+" ) {
+      alert(message + (Number(firstNumber) + Number(secondNumber)))
+       } else if (signs === "-") {
+      alert(message + (Number(firstNumber) - Number(secondNumber)))
+       } else if (signs === "*") {
+        alert(message + (Number(firstNumber) * Number(secondNumber)))
+       } else if (signs === "/") {
+        alert(message + (Number(firstNumber) / Number(secondNumber)))
+       } 
+    
+      
+      
+      
+      
+    
+    
+    
+    // if (answer == 10) {
+    //   alert("Wow! Correct!");
+    // } else {
+    //   alert("Wrong!")
+    // }
 
 
 
