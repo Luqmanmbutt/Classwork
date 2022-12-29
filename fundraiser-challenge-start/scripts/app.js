0bf943b494df6587726df0572d5c9bdc30420ec3
Luqman Butt <luqmanmbutt1@gmail.com>
2022-12-29 21:26:55 +0000

    created a button that adds 1 each time you click it

--- fundraiser-challenge-start/scripts/app.js
+++ fundraiser-challenge-start/scripts/app.js
@@ -4,12 +4,25 @@ function init() {
  
 
 
+let targetBar = 0 
+
+
+
+  const btn = document.querySelector(".one-pound-button")
+  console.log("btn is working", btn)
+    
+  const donateButton = () => {
+    // console.log('donate button is working')
+    targetBar++
+    console.log("target button", targetBar)
+        
+}
 
 
 
 
 
-
+btn.addEventListener("click", donateButton)
 
 
 
