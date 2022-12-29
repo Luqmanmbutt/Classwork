c2cf37c027439949f8d34b802a98ea8b90028cdf
Luqman Butt <luqmanmbutt1@gmail.com>
2022-12-29 19:19:44 +0000

    wireframe done for donation challenge

---
+++ fundraiser-challenge-start/scripts/app.js
@@ -0,0 +1,34 @@
+// Remember to console log and check if your JS is connected properly
+
+function init() {
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+window.addEventListener('DOMContentLoaded', init)
