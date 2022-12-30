be4278909feef10a2380231c5e006249f7e374a2
Luqman Butt <luqmanmbutt1@gmail.com>
2022-12-30 14:31:12 +0000

    logic for width for loading bar works

--- fundraiser-challenge-start/scripts/app.js
+++ fundraiser-challenge-start/scripts/app.js
@@ -6,33 +6,41 @@ const onePoundButton = document.querySelector(".one-pound-button")
 const fivePoundButton = document.querySelector(".five-pound-button")
 const tenPoundButton = document.querySelector(".ten-pound-button")
 const donationBar = document.querySelector(".donation-target")
+const loadingBar = document.querySelector(".loading-bar")
 
-donationBar.style.backgroundColor = "black" 
+console.log("loadingBar", loadingBar)
+
+donationBar.style.backgroundColor = "black"
+loadingBar.style.width = "0%"
 
 let targetBar = 0 
 
 
-
 const onePoundBtn = () => {
-  // console.log('donate button is working')
-  targetBar ++ 
+  targetBar += 1
+  loadingBar.style.width = targetBar + "%"
   console.log("target button", targetBar)
       
 }
 
 const fivePoundBtn = () => {
   targetBar += 5
+  loadingBar.style.width = targetBar + "%"
   console.log("target button", targetBar)
 
 }
 
 const tenPoundBtn = () => {
   targetBar += 10
+  loadingBar.style.width = targetBar + "%"
   console.log("target button", targetBar)
 
 }
 
-
+const targetReached = () => {
+  targetBar = 100
+  
+}
 
 
 
