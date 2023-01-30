e3609e8f0fb150f95ea3a77691c74d6cdcfbded1
Luqman Butt <luqmanmbutt1@gmail.com>
2023-01-30 20:23:41 +0000

    created a react app with a clean up of all the files, created components which hold a specific API, grabbed one piece of info from each data to show it on the home page

--- react-cw-part-2/src/components/NavigationBar.js
+++ react-cw-part-2/src/components/NavigationBar.js
@@ -1,6 +1,17 @@
 import React from 'react'// first thing you do is import react
+// write the name of the function excatly the same way as components 
+const NavigationBar = () => {
+  let obj = {
+    name: 'julie',
+    age: '56'
+  }
 
-const NavigationBar = () => { // write the name of the function excatly the same way as components 
+  let array = [0, 1, 2]
+  let newArray = [ ...array, 5]
+  console.log('newarray ', newArray)
+
+  const newObj = { ...obj, address: 'london'}
+    console.log('newObj', newObj)
   return (
     <>
       <div className="navbar-container">  
